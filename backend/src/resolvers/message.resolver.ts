14b7dc691f95a579dcde1f0983661aec8238b09a
PabloRSantos <pablorsantos15@gmail.com>
2020-11-18 22:35:02 -0200

    feat: message and user resolvers

---
+++ backend/src/resolvers/message.resolver.ts
@@ -0,0 +1,45 @@
+
+import { Args, Mutation, Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
+import Message from 'src/db/models/message.entity';
+import User from 'src/db/models/user.entity';
+import RepoService from 'src/repo.service';
+import MessageInput from './input/message.input';
+
+@Resolver(() => Message)
+class MessageResolver {
+   constructor(private readonly repoService: RepoService) {}
+
+  @Query(() => [Message])
+  public async getMessages(): Promise<Message[]> {
+    return this.repoService.messageRepo.find();
+  }
+  @Query(() => Message, {nullable: true})
+  public async getMessagesFromUser(@Args('user_id') userId: number): Promise<Message[]> {
+    return  this.repoService.messageRepo.find({
+        where: {
+            userId
+        }
+    });
+  }
+
+  @Query(() => Message, { nullable: true })
+  public async getMessage(@Args('id') id: number): Promise<Message> {
+      return this.repoService.messageRepo.findOne(id)
+  }
+
+  @Mutation(() => Message)
+  public async createMessage(@Args('data') input: MessageInput): Promise<Message> {
+      const message = this.repoService.messageRepo.create({ 
+          content: input.content,
+          userId: input.userId
+        });
+
+      return this.repoService.messageRepo.save(message);
+  }
+
+  @ResolveField(() => User, { name: 'user' })
+  public async getUser(@Parent() parent: Message): Promise<User> {
+      return this.repoService.userRepo.findOne(parent.userId)
+  }
+}
+export default MessageResolver;
